8a6ddb325079355ab99ce7287ef74298f8a97b2a
mkruglov <mrmarkussss@gmail.com>
2023-11-28 17:54:47 +0200

    New files and renames

--- src/test/java/org/example/StatusCodeTest.java
+++ src/test/java/org/example/StatusCodeTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 @Log4j2
-public class StatusCodeTests extends BaseTest {
+public class StatusCodeTest extends BaseTest {
 
     @AfterMethod(description = "Log symbols")
     public void logDivision(){
